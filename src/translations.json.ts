c3bf9c49cf7396658b403161b76f1fb11e416a48
Celina-IJS <celina.dev@instajs.com>
2024-11-27 17:17:07 +0700

    update translations

---
+++ src/translations.json.ts
@@ -0,0 +1,31 @@
+export default {
+  en: {
+    "whats_on_your_mind_today": "What's on your mind today?",
+    "whats_happening": "What's happening?",
+    "pin_note": "Pin note",
+    "unpin_note": "Unpin note",
+    "delete_note": "Delete note",
+    "cancel": "Cancel",
+    "post_your_thoughts": "Post your thoughts...",
+    "edit": "Edit",
+    "are_you_sure": "Are you sure?",
+    "do_you_really_want_to_delete_this_note": "Do you really want to delete this note?",
+    "post": "Post",
+  },
+  "zh-hant": {
+
+  },
+  "vi": {
+    "whats_on_your_mind_today": "Hôm nay bạn đang nghĩ gì?",
+    "whats_happening": "Chuyện gì đang xảy ra?",
+    "pin_note": "Ghim bài đăng",
+    "unpin_note": "Bỏ ghim bài đăng",
+    "delete_note": "Xóa bài đăng",
+    "cancel": "Hủy",
+    "post_your_thoughts": "Đăng bình luận của bạn...",
+    "edit": "Chỉnh sửa",
+    "are_you_sure": "Bạn có chắc chắn không?",
+    "do_you_really_want_to_delete_this_note": "Bạn có chắc chắn muốn xóa bài đăng này không?",
+    "post": "Đăng bài",
+  }
+}
